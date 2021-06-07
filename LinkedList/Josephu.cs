2da868fa9e819cf0b4719c0aded4042e950de4fb
GAME\wb.liujin01 <2068699719@qq.com>
2021-06-07 09:43:34 +0800

    C# 循环列表实现约瑟夫算法

--- LinkedList/Josephu.cs
+++ LinkedList/Josephu.cs
@@ -10,7 +10,8 @@ namespace DataStruct.LinkedList
         {
             CircleSingleLinkedList csll = new CircleSingleLinkedList();
             csll.AddBoy(5);
-            csll.ShowBoy();
+            //csll.ShowBoy();
+            csll.CountBoy(1, 2, 5);
         }
     }
 
@@ -62,6 +63,46 @@ namespace DataStruct.LinkedList
                 }
                 curBoy = curBoy.next;
             }
+        }
+
+        /// <summary>
+        /// 约瑟夫算法
+        /// </summary>
+        /// <param name="startNo">表示从第几个小孩开始数数</param>
+        /// <param name="countNum">表示数几下</param>
+        /// <param name="num">表示最初有多少小孩在圈中</param>
+        public void CountBoy(int startNo,int countNum,int num)
+        {
+            if(first == null || startNo < 1 || startNo > num)
+            {
+                Console.WriteLine("参数输入有误，请重新输入");
+                return;
+            }
+            // 将helper 指针指向first的前面
+            Boy helper = first;
+            while (helper.next != first)
+            {
+                helper = helper.next;
+            }
+            // 报数前移动到k-1 次的位置上
+            for (int i = 0; i < startNo - 1; i++)
+            {
+                first = first.next;
+                helper = helper.next;
+            }
+            // 开始报数 helper 和 first 同时移动到需要出圈的小孩位置上
+            while(helper != first)
+            {
+                for (int i = 0; i < countNum - 1; i++)
+                {
+                    first = first.next;
+                    helper = helper.next;
+                }
+                Console.WriteLine("出圈序列："+first.no);
+                first = first.next;
+                helper.next = first;
+            }
+            Console.WriteLine("出圈序列：" + first.no);
         }
     }
 
