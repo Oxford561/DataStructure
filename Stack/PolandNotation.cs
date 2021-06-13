dab8dfd5b2aa881d4206690db28e88bb484792cd
Oxford561 <18924192170@189.cn>
2021-06-13 09:59:58 +0800

    完成逆波兰表达式代码，中缀表达式转成后缀表达式

--- Stack/PolandNotation.cs
+++ Stack/PolandNotation.cs
@@ -9,16 +9,81 @@ namespace DataStruct.Stack
     {
         public static void Main(string[] args)
         {
-            //string expression = "1+((2+3)*4)-5";
-            //List<string> infixExpressionList = ToInfixExpressionList(expression);
-
-
+            string expression = "1+((2+3)*4)-5";
+            List<string> infixExpressionList = ToInfixExpressionList(expression);
+            List<string> parseSuffixExpressionList = ParseSuffixExpressionList(infixExpressionList);
+            int res = Calculate(parseSuffixExpressionList);
+            Console.WriteLine("1+((2+3)*4)-5=" + res);
             // 定义逆波兰表达式,为了方便，数字和符号用空格隔开
             // （3+4）*5-6   =》 3 4 + 5 * 6 - 
-            string suffixExpression = "3 4 + 5 * 6 -";
-            List<string> rpnList = GetListString(suffixExpression);
-            int res = Calculate(rpnList);
-            Console.WriteLine("（3+4）*5-6 =" + res);
+            //string suffixExpression = "3 4 + 5 * 6 -";
+            //List<string> rpnList = GetListString(suffixExpression);
+            //int res = Calculate(rpnList);
+            //Console.WriteLine("（3+4）*5-6 =" + res);
+        }
+
+        public static List<string> ParseSuffixExpressionList(List<string> ls)
+        {
+            Stack<string> s1 = new Stack<string>();
+            List<string> s2 = new List<string>();
+            foreach (var item in ls)
+            {
+                if (IsNumberic(item))
+                {
+                    s2.Add(item);
+                }else if (item.Equals("("))
+                {
+                    s1.Push(item);
+                }else if (item.Equals(")"))
+                {
+                    while (!s1.Peek().Equals("("))
+                    {
+                        s2.Add(s1.Pop());
+                    }
+                    s1.Pop();//将 ( 弹出 消除括号
+                }
+                else
+                {
+                    // 当 item的优先级小于等于 s1栈顶运算符
+                    // 弹出s1 栈顶运算符加入s2
+                    while(s1.Count != 0 && GetProtor(item) <= GetProtor(s1.Peek()))
+                    {
+                        s2.Add(s1.Pop());
+                    }
+                    s1.Push(item);
+                }
+            }
+
+            while(s1.Count != 0)
+            {
+                s2.Add(s1.Pop());
+            }
+
+            return s2;
+        }
+
+        public static int GetProtor(string operation)
+        {
+            int res = 0;
+            switch (operation)
+            {
+                case "+":
+                    res = 1;
+                    break;
+                case "-":
+                    res = 1;
+                    break;
+                case "*":
+                    res = 2;
+                    break;
+                case "/":
+                    res = 2;
+                    break;
+                default:
+                    Console.WriteLine("不存在该运算符");
+                    break;
+            }
+            return res;
         }
 
         // 将中缀表达式转成对于的list
