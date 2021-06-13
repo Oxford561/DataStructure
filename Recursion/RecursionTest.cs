ac45fbab1e7256a2534a3986c227051b91a26937
Oxford561 <18924192170@189.cn>
2021-06-13 10:56:06 +0800

    熟悉迭代原理，实现测试示例

---
+++ Recursion/RecursionTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStruct.Recursion
+{
+    public class RecursionTest
+    {
+        public static void Main(string[] args)
+        {
+            Test(4);
+            Factorial(4);
+        }
+        
+        //打印问题
+        public static void Test(int n)
+        {
+            if(n > 2)
+            {
+                Test(n - 1);
+            }
+            Console.WriteLine("n=" + n);
+        }
+
+        public static int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Factorial(n - 1) * n;
+            }
+        }
+    }
+}
