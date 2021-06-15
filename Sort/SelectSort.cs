d7e9821e1c8d5e62e75c73591d44ff1a2cd3bf00
Oxford561 <18924192170@189.cn>
2021-06-15 21:31:41 +0800

    实现选择排序算法

---
+++ Sort/SelectSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStruct.Sort
+{
+    // O(n^2)
+    public class SelectSort
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 101,34,119,1,-1,90,123};
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                int minIndex = i;
+                int min = arr[i];
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if(min > arr[j])
+                    {
+                        min = arr[j];
+                        minIndex = j;
+                    }
+                }
+                if(minIndex != i)
+                {
+                    arr[minIndex] = arr[i];
+                    arr[i] = min;
+                }
+            }
+
+            Console.WriteLine("排序后：");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            } 
+        }
+    }
+}
