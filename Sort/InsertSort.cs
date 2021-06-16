85e76acfc5ea96386ebf9e9220ac47b9dae8c5fe
GAME\wb.liujin01 <2068699719@qq.com>
2021-06-16 09:22:48 +0800

    实现插入排序算法

---
+++ Sort/InsertSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStruct.Sort
+{
+    public class InsertSort
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 101, 34, 119, 1, -1, 89 };
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int insertVal = arr[i];
+                int insertIndex = i - 1;
+                while (insertIndex >= 0 && insertVal < arr[insertIndex])
+                {
+                    arr[insertIndex + 1] = arr[insertIndex];
+                    insertIndex--;
+                }
+                arr[insertIndex + 1] = insertVal;
+            }
+            Console.WriteLine("排序后：");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine(" ");
+        }
+    }
+}
