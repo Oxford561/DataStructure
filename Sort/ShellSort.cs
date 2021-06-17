60af315f914e1ec77e9955b91511615adb02a659
GAME\wb.liujin01 <2068699719@qq.com>
2021-06-17 09:10:30 +0800

    实现希尔排序的交换法和移动法

---
+++ Sort/ShellSort.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStruct.Sort
+{
+    public class ShellSort
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 8, 9, 1, 7, 2, 3, 5, 4, 6, 0 };
+            int temp = 0;
+            // 希尔排序交换法
+            //for (int gap = arr.Length/2; gap > 0; gap/=2)
+            //{
+            //    for (int i = gap; i < arr.Length; i++)
+            //    {
+            //        for (int j = i - gap; j >= 0; j-=gap)
+            //        {
+            //            if (arr[j]>arr[j+gap])
+            //            {
+            //                temp = arr[j];
+            //                arr[j] = arr[j + gap];
+            //                arr[j + gap] = temp;
+            //            }
+            //        }
+            //    }
+            //}
+
+            // 希尔排序移动法
+            for (int gap = arr.Length / 2; gap > 0; gap /= 2)
+            {
+                for (int i = gap; i < arr.Length; i++)
+                {
+                    int j = i;
+                    temp = arr[j];
+                    if (arr[j] < arr[j - gap])
+                    {
+                        while(j-gap >=0 && temp < arr[j - gap])
+                        {
+                            arr[j] = arr[j - gap];
+                            j -= gap;
+                        }
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("排序后的数组：");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
